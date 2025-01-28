a45a8cfbc87971da5e42e74f01db5d576b026f27
VictorCunha26 <vicentinivictor130@gmail.com>
2025-01-28 13:25:03 -0300

    commit

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from modelos.avengers import Avengers
-from modelos.interface import Interface as i
-# from modelos.database import Database
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/ola")
+def hello_world():
+    return "<p>Olá, mundo!</p>"
+
+@app.route("/")
+def main_page():
+    return render_template("home.html")
+
+# from modelos.avengers import Avengers
+# from modelos.interface import Interface as i
+# # from modelos.database import Database
 
 
-def main():
+# def main():
 
-   Avengers.carregar_herois()
-   i.apresentar_menu_principal()
+#    Avengers.carregar_herois()
+#    i.apresentar_menu_principal()
 
-if __name__ == '__main__': # sempre no final do arquivo contendo a definição da função principal
-    main()
+# if __name__ == '__main__': # sempre no final do arquivo contendo a definição da função principal
+#     main()
